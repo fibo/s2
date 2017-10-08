b38be3f3d7f48d5abb77d77a0f6fb827f5c02c25
Gianluca Casati <fibo@users.noreply.github.com>
2017-10-08 23:56:53 +0200

    added Conj

--- circle.go
+++ circle.go
@@ -2,11 +2,11 @@ package s2
 
 // Circles and lines are the same in this world. They are represented by
 // an hermitian 2x2 matrix (a, b, c, d) hence b = math.Conj(c) and
-// a and d are real numbers
+// a and d are real numbers. Lines has a = 0.
 type Circle struct {
-	a float64
-	c complex128
-	d float64
+	A float64
+	C complex128
+	D float64
 }
 
 // TODO circle.Inv() and point.Inv() computes inversive geometry transform.
--- point_test.go
+++ point_test.go
@@ -4,10 +4,27 @@ import (
 	"testing"
 )
 
-var z1 = Point{1, 2}
-var z2 = Point{2, 4}
+var z1 = Point{1 + 1i, 2}
+var z2 = Point{2 + 2i, 4}
+var z3 = Point{1 - 1i, 2}
+var z4 = Point{2 - 2i, 4}
 var inf = Infinity()
 
+func TestConj(t *testing.T) {
+	cases := []struct {
+		a Point
+		b Point
+	}{
+		{z1, z3},
+		{z2, z4},
+	}
+
+	for _, point := range cases {
+		if !Eq(Conj(point.a), point.b) {
+			t.Errorf("Point %v is not conjugate of %v", point.a, point.b)
+		}
+	}
+}
 func TestEq(t *testing.T) {
 	cases := []struct {
 		a Point
