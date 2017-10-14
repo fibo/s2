e1f6230e5f211c0183da8432df7eaa5c29b04f1b
Gianluca Casati <fibo@users.noreply.github.com>
2017-10-14 19:51:07 +0200

    better Eq algorithm, do not normalize

--- package.go
+++ package.go
@@ -1,3 +1,3 @@
 // Package s2 implements the Riemann Sphere geometry.
-// For info point your browser to http://g14n.info/s2
+// Point your browser to http://g14n.info/m2c for more info.
 package s2
