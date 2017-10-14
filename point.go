701f7acf4369f95c617707edff137c9010705cf1
fibo <casati_gianluca@yahoo.it>
2017-10-14 19:51:07 +0200

    better Eq algorithm, do not normalize

--- point.go
+++ point.go
@@ -11,21 +11,34 @@ type Point struct {
 	Z, W complex128
 }
 
-var conj = cmplx.Conj
+// NewPoint is a Point constructor. It only takes one Z argument, since
+// W defaults to 1, i.e. the point is already normalized.
+func NewPoint(Z complex128) *Point {
+	return &Point{Z, 1}
+}
 
 // Conj returns the conjugated point.
 func Conj(p Point) Point {
-	q := Point{conj(p.Z), conj(p.W)}
-	q.Norm()
-	return q
+	return Point{cmplx.Conj(p.Z), cmplx.Conj(p.W)}
 }
 
 // Eq checks that two points are equal.
 func Eq(p Point, q Point) bool {
-	p.Norm()
-	q.Norm()
-
-	return (p.Z == q.Z) && (p.W == q.W)
+	if p.W == 0 {
+		if q.W == 0 {
+			// Both p, q are Infinity.
+			return true
+		} else {
+			return p.Z*q.W == p.W*q.Z
+		}
+	} else {
+		if q.W == 0 {
+			// Point q is Infinity, but p is not.
+			return false
+		} else {
+			return p.Z*q.W == p.W*q.Z
+		}
+	}
 }
 
 // Infinity returns the point at infinity, i.e. with w = 0.
@@ -33,13 +46,12 @@ func Infinity() Point {
 	return Point{1, 0}
 }
 
-// LFT computes a Linear fraction transformation (https://en.wikipedia.org/wiki/Linear_fractional_transformation)
+// LFT computes a Linear fractional transformation (https://en.wikipedia.org/wiki/Linear_fractional_transformation).
 func LFT(p Point, m m2c.Matrix) Point {
-	q := Point{m.A*p.Z + m.B*p.W, m.C*p.Z + m.D*p.W}
-	return q
+	return Point{m.A*p.Z + m.B*p.W, m.C*p.Z + m.D*p.W}
 }
 
-// Norm normalizes a point in the form (z, w) to the form z or Infinity.
+// Norm normalizes a point in the form (z, w) to the form z' or Infinity.
 func (p *Point) Norm() {
 	if p.W == 0 {
 		p.Z = 1
