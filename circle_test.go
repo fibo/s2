987eba8875063485bb689878b0492c06a5876b3d
fibo <casati_gianluca@yahoo.it>
2017-10-15 22:04:12 +0200

    better PointEq func

---
+++ circle_test.go
@@ -0,0 +1,27 @@
+package s2
+
+import (
+	"testing"
+)
+
+func TestInv(t *testing.T) {
+	var unit1 = NewCircle(1, 0, 1)
+	var unit2 = NewCircle(1, 0, 1)
+	var unit3 = NewCircle(1, 0, 1)
+
+	cases := []struct {
+		in        Circle
+		out       Circle
+		inversion Circle
+	}{
+		{unit1, unit2, unit3},
+	}
+
+	for _, circle := range cases {
+		circle.in.Inv(circle.inversion)
+
+		if !CircleEq(circle.out, circle.in) {
+			t.Errorf("Circle %v is not the inversion of %v", circle.out, circle.in)
+		}
+	}
+}
